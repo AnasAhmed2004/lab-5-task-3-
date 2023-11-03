f95edd783209dafd07943630449a73e89c8a9b6f
AnasAhmed2004 <148556445+AnasAhmed2004@users.noreply.github.com>
2023-11-03 16:27:42 +0100

    Add files via upload

---
+++ Lab5.java
@@ -0,0 +1,27 @@
+public class Lab5 {
+    public static void main(string[] args) {
+        int n = 10, i, j, sum = 0;
+        int matrix[][] = new int[n][n];
+        I
+
+                for(i = 0; i < matrix.length; i++){
+                    for(j=0; j < matrix.length; j++){
+                        if(i == j) {
+                            matrix[i][j] = i;
+                            sum += matrix[i][j];
+                        }
+                        else{
+                            matrix[i][j] + " ");
+
+                        }
+                        system.out.print(matrix[i][j] + " ");
+                    }
+                    system.out.print();
+
+
+                }
+        system.out.print();
+        system.out.print("the sum of highlighted elements in the arrea s equal to: " + sum);
+
+    }
+}
